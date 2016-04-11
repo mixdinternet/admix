ead8e2bb35783b63c318c35ed5e345ab66980005
Irineu Martins Junior <irineu@mixd.com.br>
2016-04-11 21:00:39 +0000

    Acrescentar redirecionamento de www

--- config/admin.php
+++ config/admin.php
@@ -1,6 +1,23 @@
 <?php
 
 return [
+
     'url' => 'admix',
-    'name' => '<b>MIXD</b> Internet'
+
+    'name' => '<b>MIXD</b> Internet',
+
+    'www' => true,
+
+    /**
+     * strings encontradas na url que não deixará redirecionar para o www
+     */
+    'skipWww' => [
+        '.mixd'
+        , '.local'
+        , '.dev'
+        , 'localhost'
+    ],
+
+    'protocol' => 'http'
+
 ];
