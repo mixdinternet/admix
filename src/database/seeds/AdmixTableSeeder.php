26d6717e030beb691602324047bac72bad896328
Irineu Junior <irineu@mixd.com.br>
2016-10-11 14:21:53 +0000

    Renomeando Seeder / Bcrypt

--- src/database/seeds/AdmixTableSeeder.php
+++ src/database/seeds/AdmixTableSeeder.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
 use Mixdinternet\Admix\User;
 
-class UserTableSeeder extends Seeder
+class AdmixTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -15,13 +15,13 @@ class UserTableSeeder extends Seeder
     {
         Model::unguard();
 
-        #DB::table('roles')->truncate();
-
+        DB::table('roles')->truncate();
         DB::table('users')->truncate();
 
         $email = strtolower(str_random(6) . '@example.' . str_random(3));
         $password = str_random(8);
-        User::create(['name' => 'Administrador', 'email' => $email, 'password' => $password]);
+        User::create(['name' => 'Administrador', 'email' => $email, 'password' => bcrypt($password)]);
+
         $this->command->info("Usuário administrador criado.");
         $this->command->info("E-mail => $email");
         $this->command->info("Senha => $password");
